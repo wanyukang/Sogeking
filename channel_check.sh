ad4ae6312a87bd0cc8ad2144b566e329bb147e29
wanyukang <36562273@qq.com>
2016-04-15 14:54:13 +0800

    modified channel_check.sh script for zgb

--- channel_check.sh
+++ channel_check.sh
@@ -3,16 +3,22 @@
 echo "=============== Start Check ! ===============\n"
 #渠道号
 channels="
-A7420 
-A99 
-A999
+yyb
+360sjzs 
+xmyy
+wdj
+91
+azsc
+bdsjzs
+jingdong
 "
-version="5.1.5"
+#version="5.1.5"
 
 for channel in $channels
 do
     
-	actual="`aapt dump xmltree ./apk/yidao_${version}_${channel}_release.apk AndroidManifest.xml|awk '/android:value/ { print $4 }'|sed -e 's/"//'|sed -e 's/")//'|sed -n '1p'`"
+	actual="`unzip -l ./apk/什么值得膜-${channel}.apk|awk '/cztchannel_*/ {print $4}'|awk -F _ '{print $2}'`"
+	#actual="`aapt dump xmltree ./apk/yidao_${version}_${channel}_release.apk AndroidManifest.xml|awk '/android:value/ { print $4 }'|sed -e 's/"//'|sed -e 's/")//'|sed -n '1p'`"
 	except=${channel}
 	date="`date|tr -d ' '`"
 	# echo ${date}
